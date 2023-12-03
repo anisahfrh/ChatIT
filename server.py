51b50a29ff62f3502ce765e0acb9c52bc9a2c963
anisahfrh <anisahfarah28@gmail.com>
2023-12-04 03:20:33 +0700

    add server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def run_rasa_server():
+    # Specify the path to your Rasa model
+    model_path = "chatbot\models\20231202-190337-rectilinear-meter.tar.gz"
+
+    # Command to run the Rasa server in API mode
+    command = f"rasa run --enable-api -m {model_path}"
+
+    # Use subprocess to run the Rasa server
+    subprocess.run(command, shell=True)
+
+if __name__ == "__main__":
+    run_rasa_server()
