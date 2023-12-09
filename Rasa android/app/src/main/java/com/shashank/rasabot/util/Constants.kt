7f4d55a16e6597edca13de894f3719382d9f60b4
unknown <jovansurya97@gmail.com>
2023-12-09 09:57:21 +0700

    tambah app

---
+++ Rasa android/app/src/main/java/com/shashank/rasabot/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.shashank.rasabot.util
+
+class Constants {
+
+    companion object {
+        val NGROCK_URL = "https://c4f6-103-94-190-20.ngrok.io"
+        val BASE_URL = "$NGROCK_URL/webhooks/rest/"
+        val NETWORK_TIMEOUT = 5000L
+        val MESSAGE_TEXT_NULL = "NA"
+
+        val USER = 0
+        val BOT = 1
+        val LOADING = 2
+    }
+}
